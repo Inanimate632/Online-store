cb8f275f2d89bce7643eb553802fd4e5784925de
Artem sssss <inanmate631@gmail.com>
2022-12-14 16:06:31 +0200

    feat: add webpack config and eslint

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    entry: './Store/main',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.(js)$/, use: 'babel-loader' },
+            { test: /.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource', },
+            {
+                test: /\.mp3$/,
+                type: 'asset'
+            },
+            { test: /\.ts$/i, use: 'ts-loader' },
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './Store/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+        new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' })
+    ],
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+}
