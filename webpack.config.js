295d977ea66944ddc955595e2372addb062e3163
Artem sssss <inanmate631@gmail.com>
2022-12-15 03:38:04 +0200

    feat: add scss module

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,17 @@ module.exports = {
                 test: /\.html$/i,
                 loader: "html-loader",
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
             { test: /\.css$/, use: ['style-loader', 'css-loader'] },
             { test: /\.(js)$/, use: 'babel-loader' },
             { test: /.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource', },
