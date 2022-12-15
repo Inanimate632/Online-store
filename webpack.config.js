0e952631b4b9136434cbcb22732e8d233922860d
Diana Pitalenko <dpitalenko27@gmail.com>
2022-12-15 16:57:40 +0400

    refactor: change folder name and tab size

--- webpack.config.js
+++ webpack.config.js
@@ -5,52 +5,64 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const EslingPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-    entry: './Store/main',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    "style-loader",
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
-                ],
-            },
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-            { test: /\.(js)$/, use: 'babel-loader' },
-            { test: /.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource', },
-            {
-                test: /\.mp3$/,
-                type: 'asset'
-            },
-            { test: /\.ts$/i, use: 'ts-loader' },
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './Store/index.html',
-            filename: './index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-        }),
-        new CleanWebpackPlugin(),
-        new EslingPlugin({ extensions: 'ts' })
-    ],
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+  entry: './src/main',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      { 
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(js)$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.mp3$/,
+        type: 'asset'
+      },
+      {
+        test: /\.ts$/i,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+      extensions: ['.ts', '.js']
+  },
+  plugins: [
+      new HtmlWebPackPlugin({
+          template: './src/index.html',
+          filename: './index.html'
+      }),
+      new MiniCssExtractPlugin({
+          filename: '[name].css',
+          chunkFilename: '[id].css',
+      }),
+      new CleanWebpackPlugin(),
+      new EslingPlugin({ extensions: 'ts' })
+  ],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
 }
